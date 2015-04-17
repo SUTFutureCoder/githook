b5e3e3bfa924a29159bc0b5d95a48d345c58c853
linxingchen <linxingchen@baidu.com>
2015-04-17 12:29:19 +0800

    INIT commit

---
+++ githook.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * A simple exec file can exec git pull from gitlab by Web Hooks
+ * 
+ * @author *Chen Lin
+ */
+
+// Include configuration file
+require __DIR__ . '/config.user.php';
+
+//Prepare error var
+$error = '';
+
+//Prepare payload array
+$payload = array();
+
+//Get param from POST request and get the repo info
+$fileData = '---' . date('Y-m-d H:i:s') . '---' . PHP_EOL;
+$fileData .= '----------' . PHP_EOL;
+$fileData .= 'Repo check' . PHP_EOL;
+$fileData .= '----------' . PHP_EOL;
+
+if (empty($error) && !empty($_REQUEST['payload'])){
+    $payload = json_decode($_REQUEST['payload']);
+    $fileData .= 'Payload decoded:' . PHP_EOL . var_dump($payload) . PHP_EOL;
+} else {
+    $error = 'Payload variable does not exist or empty' . PHP_EOL;
+    $fileData .= $error;
+}
+
+if (empty($error)){
+    $updated = false;
+    
+    $repo_name = substr(strrchr($payload['repository']['full_name'], '/'), 1);
+    
+    $repo_clone_url = $payload['repository']['clone_url'];
+    
+    //Cache project (clone if not exists or sync)
+    if (!is_dir(__CACHE_DIR__ . $repo_name)){
+        $fileData .= '----------' . PHP_EOL;
+        $fileData .= 'Create dir' . PHP_EOL;
+        $fileData .= '----------' . PHP_EOL;
+        mkdir(__CACHE_DIR__ . $repo_name, 0777, TRUE);
+    }
+
+    $fileData .= '----------' . PHP_EOL;
+    $fileData .= 'Check out & sync' . PHP_EOL;
+    $fileData .= '----------' . PHP_EOL;
+    
+    //If dir is already created, try to sync it
+    if (is_dir(__CACHE_DIR__ . $repo_name)){
+        $fileData .= '----------' . PHP_EOL;
+        $fileData .= 'Sync' . PHP_EOL;
+        $fileData .= '----------' . PHP_EOL;
+        $command = 'cd ' . __CACHE_DIR__ . $repo_name . ' && git pull';
+        $fileData .= 'Executing: ' . $command . PHP_EOL;
+        exec($command, $result);
+        $fileData .= 'Result: ' . PHP_EOL . '* ' . implode(PHP_EOL . '* ', $result) . PHP_EOL . PHP_EOL;
+        $updated = true;
+    }
+    
+    
+    if (!$updated){
+        $fileData .= '----------' . PHP_EOL;
+        $fileData .= 'Clone' . PHP_EOL;
+        $fileData .= '----------' . PHP_EOL;
+        $command = 'git clone ' . $repo_clone_url . ' ' . __CACHE_DIR__ . $repo_name;
+        exec($command, $result);
+        $fileData .= 'Result: ' . PHP_EOL . '* ' . implode(PHP_EOL . '* ', $result) . PHP_EOL . PHP_EOL;
+    }
+
+    //Exec git pull
+    
+
+    $fileData .= 'Shell Exec Command' . PHP_EOL;
+    $fileData .= $command . PHP_EOL;
+    $fileData .= 'Shell Exec Return' . PHP_EOL;
+    foreach ($out as $out_value){
+        $fileData .= $out_value . PHP_EOL;
+    }
+}
+
+if (!empty($error)){
+    $fileData .= '**ERROR** ' . $error . PHP_EOL;
+} else {
+    $fileData .= '** SYNC FINISHED **' . PHP_EOL;
+    
+}
+
+//Write the log file
+$fp = fopen('pull_log.txt', 'a');
+file_put_contents('githook.txt', $fileData . PHP_EOL, FILE_APPEND);
